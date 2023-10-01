05ea42c2728254808e493d2f5a9ee448e1a87c32
Vasilii Orekhov <vasilii.orekhov95@gmail.com>
2023-10-01 12:03:52 +0300

    fix redirect

--- server.js
+++ server.js
@@ -9,9 +9,15 @@ const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, 'dist')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
+
+app.use(express.static(__dirname + '/dist'));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile('./dist/index.html', { root: __dirname });
 });
 
 app.listen(PORT, function () {
